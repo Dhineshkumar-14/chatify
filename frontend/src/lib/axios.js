1697e91d7eea8d81879eebe4b42b8a8c33aa30c2
Dhineshkumar <dhineshkumar.velayudham@gmail.com>
2025-11-05 15:12:35 +0530

    Created single server for both frontend and backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:8083/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080/api" : "/api",
   withCredentials: true,
 });
