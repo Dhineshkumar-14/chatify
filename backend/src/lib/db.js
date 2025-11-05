407ffc35c61864bbb4eed1594a702b35db9c5904
Dhineshkumar <dhineshkumar.velayudham@gmail.com>
2025-11-05 14:54:09 +0530

    Completed the chatify

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,9 +3,8 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected :" + conn.connection.host);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log("MongoDB connection error" + error.message);
-    process.exit(1);
+    console.log("MongoDB connection error:", error);
   }
 };
